430f5561c16e0e33533bb7140bd44ab7ae959406
Kevin Wong <57011256+kduby@users.noreply.github.com>
2021-05-16 23:52:29 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,18 +8,18 @@
 //credits: https://developers.google.com/web/fundamentals/primers/service-workers
 //         https://developer.mozilla.org/en-US/docs/Web/API/Clients/claim
 
-//Register a service worker 
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', function() {
-//       navigator.serviceWorker.register('./sw.js').then(function(registration) {
-//         // Registration was successful
-//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//       }, function(err) {
-//         // registration failed :(
-//         console.log('ServiceWorker registration failed: ', err);
-//       });
-//     });
-//   }
+Register a service worker 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('./sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
 
 //Install a service worker
   self.addEventListener('install', function(event) {
